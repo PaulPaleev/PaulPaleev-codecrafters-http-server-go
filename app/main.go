1bfb6992c4d3244bde431943929f8702eab85fd2
Pavel <pavel.panteleev@vrpconsulting.com>
2025-04-09 10:04:45 +0300

    added path check

--- app/main.go
+++ app/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -22,5 +23,11 @@ func main() {
 		os.Exit(1)
 	}
 
+	req := make([]byte, 1024)
+	conn.Read(req)
+	if !strings.HasPrefix(string(req), "GET / HTTP/1.1") {
+		conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+		return
+	}
 	conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
 }
