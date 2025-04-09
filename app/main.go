75ef3d197fb1279209b894f8c1b0938c389db8ab
Pavel <pavel.panteleev@vrpconsulting.com>
2025-04-09 15:52:04 +0300

    added file creation

--- app/main.go
+++ app/main.go
@@ -82,7 +82,7 @@ func sendNotFound(conn net.Conn) {
 
 func getEncodingsList(request string) []string {
 	var validSchemes []string
-	schemesLine := strings.Split(request, "\r\n")[3]
+	schemesLine := strings.Split(request, "\r\n")[2]
 	fmt.Println(schemesLine)
 	return validSchemes
 }
